3c58c21f7891e5a567e6faa06ec7d21e5c5c26e4
bighong <bighong12@gmail.com>
2023-11-20 01:59:16 +0900

    add data

--- lib/sheet/character.dart
+++ lib/sheet/character.dart
@@ -1,13 +1,16 @@
 import 'character_class.dart';
+import 'race.dart';
 
 class Character {
   String chracterName;
   int totalLevel;
+  Race chracterRace;
   List<CharacterClass> characterClasses;
 
   Character({
     required this.chracterName,
     required this.totalLevel,
+    required this.chracterRace,
     required this.characterClasses,
   });
 }
--- lib/sheet/spell.dart
+++ lib/sheet/spell.dart
@@ -2,27 +2,33 @@ class Spell {
   bool slotConsume;
   bool shortRest;
   bool longRest;
-  bool isConcentrate;
-  
+  bool needConcentrate;
+
   int spellLevel;
   int spellDC;
   int saveAbility;
   int damage;
   int damageType;
   int duration;
-
-  List<Condition> conditions;
+  int school;
+  int castingTime;
+  String range;
+  String spellDescription;
 
   Spell({
     required this.slotConsume,
     required this.shortRest,
     required this.longRest,
-    required this.isConcentrate,
+    required this.needConcentrate,
     required this.spellLevel,
     required this.spellDC,
     required this.saveAbility,
     required this.damage,
     required this.damageType,
     required this.duration,
+    required this.school,
+    required this.castingTime,
+    required this.range,
+    required this.spellDescription,
   });
 }
