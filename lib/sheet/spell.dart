dc04ea695df060c89c6acd0589f3b2b961f81ed9
21700185-kimtaehong <bighong12@gmail.com>
2023-11-20 11:53:36 +0900

    spell data change

--- lib/sheet/spell.dart
+++ lib/sheet/spell.dart
@@ -2,27 +2,30 @@ class Spell {
   bool slotConsume;
   bool shortRest;
   bool longRest;
-  bool isConcentrate;
+  bool needConcentrate;
   
   int spellLevel;
-  int spellDC;
   int saveAbility;
   int damage;
   int damageType;
   int duration;
+  int actionType;
+  int schoolTYpe;
 
-  List<Condition> conditions;
+  String range;
 
   Spell({
     required this.slotConsume,
     required this.shortRest,
     required this.longRest,
-    required this.isConcentrate,
+    required this.needConcentrate,
     required this.spellLevel,
-    required this.spellDC,
     required this.saveAbility,
     required this.damage,
     required this.damageType,
     required this.duration,
+    required this.actionType,
+    required this.schoolTYpe,
+    required this.range,
   });
 }
